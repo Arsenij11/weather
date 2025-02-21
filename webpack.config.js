c189305ff15ad91ac8535077d95e09371097affa
Arseniy <burkoff.ars@yandex.ru>
2025-02-21 14:10:26 +0300

    Weather

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,8 @@ module.exports = {
                 }
             },
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
             {
                 test: /\.(jpg|png|svg|jpeg|gif)$/i,
